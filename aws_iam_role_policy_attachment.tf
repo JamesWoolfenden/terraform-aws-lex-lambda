ed45c0814f4dec98ba227a2a157d6a55489ae9b3
james woolfenden <james.woolfenden@slalom.com>
2019-02-18 11:24:48 +0000

    check for false

--- aws_iam_role_policy_attachment.tf
+++ aws_iam_role_policy_attachment.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "withintent" {
-  count      = "${var.passedrole == "" ? 1: 0}"
+  count      = "${var.passedrole == false ? 1: 0}"
   role       = "${aws_iam_role.withintent.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
