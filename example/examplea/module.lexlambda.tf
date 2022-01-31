7ee62fb6a2d86766debef229ed86425eb77671b1
James Woolfenden <James.Woolfenden@gmail.com>
2022-01-31 08:07:05 +0000

    date

--- example/examplea/module.lexlambda.tf
+++ example/examplea/module.lexlambda.tf
@@ -13,8 +13,8 @@ module "lexlambda" {
   name              = var.name
   region_name       = data.aws_region.current.name
   role_arn          = data.aws_iam_role.lambda.arn
-  kms_key_id        = aws_kms_key.forlambda.arn
   kms_master_key_id = aws_kms_key.forlambda.id
+  kms_key_id        = aws_kms_key.forlambda.id
 }
 
 
